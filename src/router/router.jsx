9b781b9dae45e70bb8f46ac4ee39b6769fd379de
OctopusGamer <michaeldiazcampo@gmail.com>
2023-03-05 23:40:48 -0500

    last one

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -11,7 +11,7 @@ export const router = createBrowserRouter ([
   
     path : "/",
     element : <Root/>,
-    errorElement : <NotFound/>,
+    errorElement : <Home/>,
     children : [
       {
         index: true,
