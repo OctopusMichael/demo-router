4d63d0b20f0a6e089c1e4752e40b2fd226a7602c
OctopusGamer <michaeldiazcampo@gmail.com>
2023-03-06 00:16:36 -0500

    last vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = process.env.NODE_ENV === 'production' ? '/demo-router/' : '/'
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base,
+  base : "/demo-router/"
 
 })
